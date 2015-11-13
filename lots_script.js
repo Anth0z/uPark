4334c2674fb675502e4af6c545f7ea4cd4c4f321
Osanna Hui <osannahui@osannas-mbp.wifi.bcit.ca>
2015-11-13 14:45:22 -0800

    update script with click function

--- lots_script.js
+++ lots_script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
+
     var allLots = [];
-    
+    $("#lotInfo").css("display", "none");
     getParkingLots();
 
     function getParkingLots(){
@@ -13,51 +14,66 @@ $(document).ready(function(){
                 console.log("Connection Successful", resp);
                 var status = resp['status'];
                 if(status == 'success') {
-
                     var lots = resp['lots'];
-                    var str = "";
-
                     for(var i in lots) {
-                        //var lot = lots[i];
-                        //str += lot['company'] + " " + lot['lot_num'] + "<br/>" + lot['address'] + "<br/>";
                         allLots.push(lots[i]);
-                        //console.log(allLots[i].address);
                         var centerDiv = document.createElement("div");
-                        centerDiv.id = allLots.length;
                         centerDiv.className = "centerdiv";
 
                         var leftDiv = document.createElement("div")
-                        leftDiv.id = allLots.length;
                         leftDiv.className = "left";
+                        
+                        var rightDiv = document.createElement("div")
+                        rightDiv.className = "right";
                         
                         var results = document.createElement("div");
                         results.className = "results";
-                        results.id = allLots.length;
+                        results.id = allLots[i].upark_id;
 
                         $(leftDiv).append("Weekday Rates: "+allLots[i].rates_weekday+"/hr" + "<br/><br/>" +"Weekend Rates: "+allLots[i].rates_weekend+"/hr");
                         
                         $(centerDiv).append("<h3>"+ allLots[i].company +" "+ allLots[i].lot_num +"</h3><br/> "+ allLots[i].address);
-                        
+
                         $(results).append(leftDiv);
                         $(results).append(centerDiv);
-                        //var allResults = document.getElementById("allResults");
-                        $(document.body).append(results);
-
+                        
+                        $("#resultsList").append(results);
+                        
+                        console.log(allLots[i]);
+                        
                     }
-                    //console.log(lots);
-                    console.log(allLots);
-                    var company = str;
+                    
                    
-                    $("#resultList").text(resp['msg']);
+                    $(".results").click(function(){
+                        console.log("click div"); 
+                        console.log(this.id);
+                        console.log(allLots);
+                        console.log(allLots[this.id].company);
+                        console.log(allLots[this.id]);
+                        $("#resultsList").css("display", "none");
+                        $("#lotInfo").css("display", "block");
+
+                        $("#lotName").html(allLots[this.id].company + allLots[this.id].lot_num);
+                        $("#lotAddress").html(allLots[this.id].address);
+                        $("#lotCity").html(allLots[this.id].city);
+                        $("#lotSpace").html(allLots[this.id].total_space);
+                        $("#lotName").html(allLots[this.id].company);
+
+                        $("#weekdayPrice").html(allLots[this.id].rates_weekday);
+                        $("#weekendPrice").html(allLots[this.id].rates_weekend);
+
+                        
+
+
+
+                        //$("#lotInfo").append("<h2>"+allLots[this.id].company+" "+allLots[this.id].lot_num+"</h2>"+allLots[this.id].address+"<br/>"+allLots[this.id].city+" "+allLots[this.id].province);
+                    });
 
                 }
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 console.log(jqXHR.statusText, textStatus);
             }
-
         });
-        
     }
-
 });
