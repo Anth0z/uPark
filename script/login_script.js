e4d02c580ecb9d261e9e65629c7df9187e8ea112
Osanna Hui <osannahui@Osannas-MacBook-Pro.local>
2015-11-15 22:32:32 -0800

    updated style and click function

--- script/login_script.js
+++ script/login_script.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
 	//loading main login page
+	$("#eula").css("display", "none");
 	$("#splash").delay(1000).fadeOut(1000);
 
 	$("#login_button").click(function(){
 		window.location.assign("index.html");
-		console.log("go to index");
 	});
 
+	$("#signup_button").click(function(){
+		window.location.assign("signup.html");
+	});
+
+	$("#signup_sub_button").click(function(){
+		$("#bg1").fadeOut(500);
+		$("#eula").delay(500).fadeIn(500);
+		$("html").css("background-color", "rgb(244, 238, 162)");
+	});
+
+	$("#web").click(function(){
+		alert("Stay tuned for our new website!");
+	});
+
+	$("#btncheck").click(function(){
+		alert("Thank you for signing up! You can now login with your username and password");
+		window.location.assign("login.html");
+	});
 
 	//submit reset email
 	$("#resetButton").click(function(){
